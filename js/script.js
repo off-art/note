67dc7ed4acef0c41b53f80d43ab27002318258c0
1ookatme <troy750@yandex.ru>
2019-10-07 19:27:12 +0300

    Добавление нового оформления для приложения 'Заметки'

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let text = document.getElementById("text");
+let butt = document.getElementById("butt");
+let list = document.getElementById("list");
+
+let notes = {};
+let edit = false;
+let key;
+
+butt.addEventListener("click", function() {
+  if (edit) {
+    notes[key] = text.value;
+    edit = false;
+    key = undefined;
+    text.value = " ";
+  } else {
+    let date = new Date();
+    let now = "Тут твоя заметка"
+    notes[now] = text.value;
+    text.value = " ";
+
+    let li = document.createElement("li");
+    li.classList.add('notes-list__link');
+    li.innerHTML = now;
+    list.appendChild(li);
+
+    li.addEventListener("click", function() {
+      text.value = notes[this.innerHTML];
+      edit = true;
+      key = this.innerHTML;
+    });
+  }
+});
+
+function removeElement()
+{
+   document.querySelector('li').remove();
+}
